34434860c855e9034ac334762909558645e5fcd2
13acid <chris.andon+github@gmail.com>
2024-02-13 22:34:48 +0000

    fetch worked! cleaned up indentation

--- music.js
+++ music.js
@@ -2,42 +2,41 @@ fetch("./music.json")
   .then((response) => response.json())
   .then((musicData) => {
 
-var music = document.getElementById("music");
-const items = musicData.items;
-for(let i=0; i < items.length; i++){
+    var music = document.getElementById("music");
+    const items = musicData.items;
+    for(let i=0; i < items.length; i++){
   
-  let item = document.createElement("div");
-  item.className = "music-item";
-  music.appendChild(item);
+      let item = document.createElement("div");
+      item.className = "music-item";
+      music.appendChild(item);
 
-  let title = document.createElement("h3");
-  title.innerHTML = items[i].title;
-  item.appendChild(title);
+      let title = document.createElement("h3");
+      title.innerHTML = items[i].title;
+      item.appendChild(title);
 
-  let artist = document.createElement("h4");
-  artist.innerHTML = items[i].artist;
-  item.appendChild(artist);
+      let artist = document.createElement("h4");
+      artist.innerHTML = items[i].artist;
+      item.appendChild(artist);
 
-  let image = document.createElement("img");
-  image.setAttribute('src', items[i].image);
-  item.appendChild(image);
+      let image = document.createElement("img");
+      image.setAttribute('src', items[i].image);
+      item.appendChild(image);
 
-  let ul = document.createElement("ul");
-  item.appendChild(ul);
+      let ul = document.createElement("ul");
+      item.appendChild(ul);
 
-  let genre = document.createElement("li");
-  genre.innerHTML = "<strong>Genre:</strong> " + items[i].genre;
-  ul.appendChild(genre);
+      let genre = document.createElement("li");
+      genre.innerHTML = "<strong>Genre:</strong> " + items[i].genre;
+      ul.appendChild(genre);
 
-  let releaseDate = document.createElement("li");
-  releaseDate.innerHTML = "<strong>Released:</strong> " + items[i].release_month + " " + items[i].release_year;
-  ul.appendChild(releaseDate);
+      let releaseDate = document.createElement("li");
+      releaseDate.innerHTML = "<strong>Released:</strong> " + items[i].release_month + " " + items[i].release_year;
+      ul.appendChild(releaseDate);
 
-  let blurb = document.createElement("li");
-  blurb.innerHTML = items[i].blurb;
-  ul.appendChild(blurb);
-}
-}
-)
+      let blurb = document.createElement("li");
+      blurb.innerHTML = items[i].blurb;
+      ul.appendChild(blurb);
+    }
+  });
 
 
