24bffb190fcea718664a2598ac172cf4f49598e7
13acid <chris.andon+github@gmail.com>
2024-02-14 00:54:27 +0000

    added slide out nav for momobile devices

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3
+function openNav() {
+  document.getElementById("side-nav").style.width = "100%";
+  document.getElementById("menu-button").style.display = "none";
+  document.getElementById("close-menu-button").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("side-nav").style.width = "0px";
+  document.getElementById("menu-button").style.display = "block";
+  document.getElementById("close-menu-button").style.display = "none";
 }
